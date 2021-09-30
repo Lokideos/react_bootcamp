09f11de19ee1a13b0123421f3800597006888510
lokideos <archmage09@gmail.com>
2021-09-30 19:46:38 +0300

    [seasons] React State system

--- reactApps/seasons/src/index.js
+++ reactApps/seasons/src/index.js
@@ -2,13 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component {
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
-    )
+  constructor(props) {
+    super(props)
 
-    return <div>Latitude: </div>
+    // The component re-renders each time state changes
+    // We only do the direct assignment to state during its initialization
+    this.state = { lat: null, errorMessage: '' };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position =>  {
+        // Always call setState() to change the state
+        this.setState({ lat: position.coords.latitude })
+      },
+      err => {
+        this.setState({ errorMessage: err.message })
+      }
+    )
+  }
+
+  render() { 
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+
+    return <div>Loading...</div>
   }
 }
 
