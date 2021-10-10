8c90829ca934bd99c123b7a8ad79292500bcac0d
lokideos <archmage09@gmail.com>
2021-10-10 10:29:51 +0300

    [blog] Redux Store Design

--- reactApps/blog/src/actions/index.js
+++ reactApps/blog/src/actions/index.js
@@ -1,11 +1,44 @@
+import _ from 'lodash'
 import jsonPlaceholder from "../apis/jsonPlaceholder"
+
+// Make sure to dispatch any other action creators
+// if u use them inside other action creators
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts())
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value()
+}
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts')
 
   dispatch({
     type: 'FETCH_POSTS',
-    payload: response
+    payload: response.data
   })
 }
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`)
+
+  dispatch({
+    type: 'FETCH_USER',
+    payload: response.data
+  })
+}
+
+// Example of _.memoize() application
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch)
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`)
+
+//   dispatch({
+//     type: 'FETCH_USER',
+//     payload: response.data
+//   })
+// })
 
